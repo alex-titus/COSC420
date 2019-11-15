33e7f41bedb94ab842a4bda60e249e9db12876cb
Alexander Roy Titus <talexander3@hpcl4-1.salisbury.edu>
2019-11-15 08:21:13 -0500

    Labs push

---
+++ lab4/slurm.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# File: myproject.sh
+
+#SBATCH --job-name=myproject
+#SBATCH --nodes=10
+#SBATCH --ntasks-per-node=6
+#SBATCH --mem=4gb
+#SBATCH --time=06:00:00    # Time limit in the form hh:mm:ss
+#SBATCH --output=out/%j.log
+
+module load mpi/openmpi3-x86_64
+
+# For a python3 project
+# srun python3 ~/Projects/myproject/myproject.py
+
+# For a C MPI project
+
+mpirun ~/COSC420/lab4/driver outputwords
