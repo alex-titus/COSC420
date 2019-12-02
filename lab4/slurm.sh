d78cbfa4067ca42bb34b46b228d30dd84d88e801
Alexander titus <talexander3@gulls.salisbury.edu>
2019-12-01 22:09:47 -0500

    Commit :)

--- lab4/slurm.sh
+++ lab4/slurm.sh
@@ -6,7 +6,7 @@
 #SBATCH --nodes=15
 #SBATCH --ntasks-per-node=10
 #SBATCH --mem=4gb
-#SBATCH --time=06:00:00    # Time limit in the form hh:mm:ss
+#SBATCH --time=24:00:00    # Time limit in the form hh:mm:ss
 #SBATCH --output=out/%j.log
 
 module load mpi/openmpi3-x86_64
