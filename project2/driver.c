ea4574317ece859ac83ce159acb6e1b653daaa59
JacksonBellinger <jdunnbellinger@gmail.com>
2019-12-05 14:58:26 -0500

    parallel parsing changes

--- project2/driver.c
+++ project2/driver.c
@@ -6,11 +6,12 @@
 #include<unistd.h>
 #include<fcntl.h>
 #include<sys/stat.h>
+#include<time.h>
+#include<mpi.h>
 
 #include "rbtree.c"
 
 
-#include <time.h>
 
 #define NB_ELEMS 10
 
@@ -19,53 +20,65 @@ int convIntToStr(char * str, int x){
   return (strlen(str));
 }
 
-MPI_Offset find_offset(int rank, int num_nodes, MPI_File* file)
+void find_offset(int rank, int num_nodes, MPI_File* file, int* chunk)
 {//sets the offset of file to where the node should start
 //and returns the length of how many bytes it should read
-	MPI_File_seek(*file, 0, MPI_SEEK_END);
-	MPI_Offset file_len;
-	MPI_File_get_size(*file, &file_len);
-	MPI_Offset starting_point = (file_len / num_nodes) * rank;
-	MPI_Offset ending_point = (file_len / num_nodes) * (rank + 1) -1;
+    MPI_File_seek(*file, 0, MPI_SEEK_END);
+    MPI_Offset file_len;
+    MPI_File_get_size(*file, &file_len);
+    MPI_Offset starting_point = (file_len / num_nodes) * rank;
+    MPI_Offset ending_point = (file_len / num_nodes) * (rank + 1) -1;
 
-	MPI_Status status;
-	char c = ' ';
+    MPI_Status status;
+    char c = ' ';
     int num_plusi = 0;
-	//printf("Node#%d starting at %d\n", rank, starting_point);
-	MPI_File_seek(*file, starting_point, MPI_SEEK_SET);
-	if(rank != 0)
-	{
-		while( num_plusi < 6 && c != 0)
-		{
+    //printf("Node#%d starting at %d\n", rank, starting_point);
+    MPI_File_seek(*file, starting_point, MPI_SEEK_SET);
+    //printf("%d start: %d\N", rank, starting_point);
+    int nbytes = 1;
+    if(rank != 0)
+    {
+        while( num_plusi < 6 && c != '\0' && nbytes >0)
+        {
+            // if(rank == 3)
+            // printf("%d: %c\n", rank, c);
             if(c=='+')
                 num_plusi++;
             else
                 num_plusi = 0;
-			//MPI_File_seek(file, -2, MPI_SEEK_CUR);
-			MPI_File_read(*file, &c, 1, MPI_CHAR, &status);
-			//printf("\tNode #%d s reading char %c\n", rank, c);
-		}
-		MPI_File_get_position(*file, &starting_point);
-	//	printf("\n\noffset before seek is %d\n", starting_point);
-		MPI_File_get_position(*file, &starting_point);
-		//printf("offset after seek is %d\n\n", starting_point);
-	}
-	//printf("Node#%d starting at %d\n", rank, starting_point);
+            //MPI_File_seek(file, -2, MPI_SEEK_CUR);
+            nbytes = MPI_File_read(*file, &c, 1, MPI_CHAR, &status);
+            //printf("\tNode #%d s reading char %c\n", rank, c);
+        }
+        MPI_File_get_position(*file, &starting_point);
+        MPI_File_seek(*file, 1, MPI_SEEK_CUR);
+    //    printf("\n\noffset before seek is %d\n", starting_point);
+        //MPI_File_get_position(*file, &starting_point);
+        //printf("offset after seek is %d\n\n", starting_point);
+    }
+    //printf("Node#%d starting at %d\n", rank, starting_point);
+    nbytes = num_plusi = 0;
+    c = ' ';
+    MPI_File_seek(*file, ending_point, MPI_SEEK_SET);
+    while( num_plusi < 6 && c != '\0' && nbytes > 0)
+    {
+        // if(rank == 3)
+        // printf("%d: %c\n", rank, c);
+        if(c=='+')
+            num_plusi++;
+        else
+            num_plusi = 0;
+        nbytes = MPI_File_read(*file, &c, 1, MPI_CHAR, &status);
+        //printf("\tNode #%d e reading char %c\n", rank, c);
+    }
+    MPI_File_seek(*file, 1, MPI_SEEK_CUR);
+    MPI_File_get_position(*file, &ending_point);
 
-	c = ' ';
-	MPI_File_seek(*file, ending_point, MPI_SEEK_SET);
-	while( c != '\n'  && c != 0)
-	{
-		MPI_File_read(*file, &c, 1, MPI_CHAR, &status);
-		//printf("\tNode #%d e reading char %c\n", rank, c);
-	}
-	MPI_File_seek(*file, -1, MPI_SEEK_CUR);
-
-	MPI_File_get_position(*file, &ending_point);
-	//printf("Node #%d ended at %d\n", rank, ending_point);//minus 2 so it goes back past the newline
-
-	MPI_File_seek(*file, starting_point, MPI_SEEK_SET);
-	return ending_point;
+    // if(rank == num_nodes)
+    //     ending_point = file_len;
+    // printf("end %d, file len : %d\n",ending_point, file_len);
+    chunk[0] = starting_point;
+    chunk[1] = ending_point;
 }
 
 // MPI_Status status;
@@ -97,6 +110,25 @@ MPI_Offset find_offset(int rank, int num_nodes, MPI_File* file)
 
 int metadataInsertion(struct word_node* root, char* meta_file)
 {
+    MPI_Comm world = MPI_COMM_WORLD;
+    int rank, world_size;
+    MPI_Comm_rank(world, &rank);
+    MPI_Comm_size(world, &world_size);
+
+    MPI_File mpi_words_file;
+    MPI_File_open(
+        world,
+        meta_file,
+        MPI_MODE_RDONLY,
+        MPI_INFO_NULL,
+        &mpi_words_file
+    );
+
+    //FILE* test = fopen("test_offset.txt", "r");
+    int chunk[2];
+    find_offset(rank, world_size, &mpi_words_file, chunk);
+    printf("node #%d start %d | end %d\n", rank, chunk[0], chunk[1]);
+
   FILE * fp;
   char * line = NULL;
   size_t len = 0;
@@ -109,7 +141,8 @@ int metadataInsertion(struct word_node* root, char* meta_file)
       printf("File not found\n");
     exit(EXIT_FAILURE);
   }
-  while ((read = getline(&line, &len, fp)) != -1) {
+  fseek(fp, chunk[0], SEEK_SET);
+  while ((read = getline(&line, &len, fp)) != -1 && ftell(fp) < chunk[1]) {
     int idLength = strlen(line)+1;
     char *id = malloc(idLength * sizeof(char));
     if (line[0] != '+'){
@@ -142,7 +175,7 @@ int metadataInsertion(struct word_node* root, char* meta_file)
       free(title);
       free(author);
 
-      int i, j;
+      int i;
       int offset = 0;
       for(i = 0; i < strlen(line);){
         int foundDelim = 0;
@@ -197,34 +230,43 @@ int metadataInsertion(struct word_node* root, char* meta_file)
   if (line){
     free(line);
   }
-  exit(EXIT_SUCCESS);
+  //exit(EXIT_SUCCESS);
+  return 0;
 }
 
 
 /* Drier program to test above function*/
-int main()
+int main(int argc, char** argv)
 {
+    MPI_Init(NULL, NULL);
+    MPI_Comm world = MPI_COMM_WORLD;
+    int rank, world_size;
+    MPI_Comm_rank(world, &rank);
+    MPI_Comm_size(world, &world_size);
+
+
     umask(0);
     srand(time(NULL));
     struct word_node *cthulu_tree = NULL;
-    clock_t t0 = clock();
-    int i;
 
     printf("proccessing search index ...\n");
-    char meta_file[100] = "./arxiv/arxiv-metadata.txt\0";
+    char meta_file[100] = "./arxiv/shortened-arxiv-metadata.txt\0";
     metadataInsertion(cthulu_tree, meta_file);
 
-    printf("Welcome to the muthah fukin game beech \n -1 to quit\n");
-    int done = 0;
-    while(!done)
+    if(rank == 0)
     {
-        char* input = malloc(100 * sizeof(char));
-        printf("Search: ");
-        scanf("%s", input);
-        printf("%s\n", input);
+        printf("Welcome to the muthah fukin game beech \n -1 to quit\n");
+        int done = 0;
+        while(!done)
+        {
+            char* input = malloc(100 * sizeof(char));
+            printf("Search: ");
+            scanf("%s", input);
+            printf("%s\n", input);
 
-        struct word_node* ret = word_search(input, cthulu_tree);
+            struct word_node* ret = word_search(input, cthulu_tree);
+        }
     }
-
+    MPI_Finalize();
     return 0;
 }
